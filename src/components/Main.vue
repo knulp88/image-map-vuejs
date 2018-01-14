fd8d1c10be1276ae65befb9896dbc703f5736c24
Chaejhjh <>
2018-01-14 16:57:06 +0900

    INIT

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+
+  .mainContainer
+    h1 {{ initMessage }}
+
+</template>
+<script>
+export default {
+  data () {
+    return {
+      initMessage: 'INIT'
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.mainContainer{
+  h1 {
+    color: gray;
+  }
+}
+</style>
